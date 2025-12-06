024db1c4e66e5cc0d2a6527896da50ff9781a8c4
KONAIN-WORSI <konainworsi@gmail.com>
2025-12-06 20:44:01 +0545

    ....

--- Week_5/PracticeLoop.java
+++ Week_5/PracticeLoop.java
@@ -2,18 +2,12 @@ package Week_5;
 
 public class PracticeLoop {
     public static void main(String[] args) {
-        // for(int i = 1; i <= 5; i++) {
-        //     for (int j = 1; j <= i; j++) {
-        //         System.out.print(j);
-        //     }
-        //     System.out.println();
-        // }   Output:
-            // 1
-            // 12
-            // 123
-            // 1234
-            // 12345
-
+        // for(int i = 1; i <= 5; i++) {             Output:                           
+        //     for (int j = 1; j <= i; j++) {        1
+        //         System.out.print(j);              12
+        //     }                                     123    
+        //     System.out.println();                 1234
+        // }                                         12345
 
 
         // for (int i = 5; i >= 1; i--) {           Output:
@@ -24,15 +18,17 @@ public class PracticeLoop {
         // }                          1                 1
 
 
-        for (int i = 1; i<=5; i++) {
-            for (int k = 1; k <= 5 - i; k++) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= i; j++) {
-                System.out.print(j);
-            }
-            System.out.println();
-        }
+        // for (int i = 1; i<=5; i++) {
+        //     for (int k = 1; k <= 5 - i; k++) { Output:
+        //         System.out.print(" ");              1
+        //     }                                      12
+        //     for (int j = 1; j <= i; j++) {        123
+        //         System.out.print(j);             1234
+        //     }                                   12345
+        //     System.out.println();
+        // }
+
+
     }
     
 }
