012e31408022392d4aeb8c90cd58f807e9a996eb
KONAIN-WORSI <konainworsi@gmail.com>
2026-02-27 16:06:22 +0545

    some changes in the rectangle file

--- week_11/Rectangle.java
+++ week_11/Rectangle.java
@@ -16,4 +16,13 @@ public class Rectangle extends Shape
     public int calculatePerimeter(int l, int b) {
         return 2 * (l + b);
     }
+
+    public void main(String[] args) {
+        Rectangle r = new Rectangle();
+        int area = r.calculateArea(5, 3);
+        int perimeter = r.calculatePerimeter(5, 3);
+        
+        System.out.println("Area of the rectangle: " + area);
+        System.out.println("Perimeter of the rectangle: " + perimeter);
+    }
 }
